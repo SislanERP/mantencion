4ace2c19a3a8cc1f9249749231169ae4527b9b69
CASENJO1992 <casenjo@landes.cl>
2020-01-23 15:33:28 -0300

    test

--- index.php
+++ index.php
@@ -41,8 +41,8 @@
             var data = new FormData(form);
 
             $.ajax({
-                url: "ajax/login.php",
                 type: "POST",
+                url: "http://mantencion.landes.cl/ajax/login.php",
                 data: data,
                 processData: false,
                 contentType: false,
