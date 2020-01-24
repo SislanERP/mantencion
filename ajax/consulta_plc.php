a414b81cfee96fcf0e5627d4c5a59f98e1e6b9d8
CASENJO1992 <casenjo@landes.cl>
2020-01-24 14:31:33 -0300

    lectura plc

--- ajax/consulta_plc.php
+++ ajax/consulta_plc.php
@@ -18,26 +18,11 @@ try
         echo "<section class='w-25 cajaplc p-4 mr-4 d-flex flex-column justify-content-center align-items-center'>";
         if($columna['log_suma'] == 1)
         {
-            try {
-                $result = $modbus->readMultipleRegisters(0, $columna['direccion'], 1);
-            } catch (Exception $e) {
-                printf("Exception: %s\n", $e);
-                return array();
-            }
-            /* split into 1 word (2 byte) chunks */
-            $result = array_chunk($result, 2);
-            /* word results */
-            $r = array();
-            for ($i = 0; $i < sizeof($result); $i++) {
-                $r[$i + $columna['direccion']] = PhpType::bytes2unsignedInt($result[$i]);
-            }
-            printf((int)$r);
-
-
-            // $recData = $modbus->readMultipleRegisters(0, $columna['direccion'], 1);
-            // //$recData[1] = $recData[1] / 1000;
-            // echo "<h1 class='tituloplc'>".$columna['nombre']."</h1></br>";
-            // echo  "<h1 class='valorplc'>".$recData[1]."</h1>";
+            
+            $recData = $modbus->readMultipleRegisters(0, $columna['direccion'], 1);
+            //$recData[1] = $recData[1] / 1000;
+            echo "<h1 class='tituloplc'>".$columna['nombre']."</h1></br>";
+            echo  "<h1 class='valorplc'>".$recData[1]."</h1>";
         }
         else
         {
