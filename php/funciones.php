5d1d54f291f9eeeb5e1117f8e5745681d2397b98
CASENJO1992 <casenjo@landes.cl>
2020-01-24 09:13:05 -0300

    test

--- php/funciones.php
+++ php/funciones.php
@@ -191,6 +191,7 @@
         $token="";
         $id_usuario = $_SESSION['id_user'];
         $token = strtok($_SERVER["REQUEST_URI"], "/");
+        echo $token;
         while($token !== false) {
             $token = strtok("/");
             $_SESSION['page'] = $token;
