c3f494932f7f15a60788d53944e2a9f8a45a7ef9
CASENJO1992 <casenjo@landes.cl>
2020-01-24 18:51:05 -0300

    envio de correo

--- php/acciones/add/add_usuario.php
+++ php/acciones/add/add_usuario.php
@@ -3,7 +3,7 @@
     include ('../../conexion.php');
 
     use PHPMailer\PHPMailer\PHPMailer;
-    require '../../vendor/autoload.php';
+    require '../../../vendor/autoload.php';
 
     $id_usuario = $_SESSION['id_user'];
     date_default_timezone_set("America/Santiago");
@@ -26,10 +26,6 @@
     $query="INSERT INTO usuarios (id_usuario,nombre,email,imagen,id_perfil,password,temporal,fec_registro,id_usuario_registro) values($contador,'$_POST[nombre0]','$_POST[email0]','img/perfil/perfil.png',$_POST[perfil0],'$hash',1,'$fecha',$id_usuario)";
     if (conectar()->query($query) === TRUE) 
     {
-        $messages[] = "Usuario guardado satisfactoriamente.";
-
-        
-
         //Crear una instancia de PHPMailer
         $mail = new PHPMailer();
         //Definir que vamos a usar SMTP
@@ -38,7 +34,7 @@
         // 0 = off (producción)
         // 1 = client messages
         // 2 = client and server messages
-        $mail->SMTPDebug  = 2;
+        $mail->SMTPDebug  = 0;
         //Ahora definimos gmail como servidor que aloja nuestro SMTP
         $mail->Host       = 'smtp.gmail.com';
         //El puerto será el 587 ya que usamos encriptación TLS
@@ -57,19 +53,35 @@
         //Esta línea es por si queréis enviar copia a alguien (dirección y, opcionalmente, nombre)
         //$mail->AddReplyTo('replyto@correoquesea.com','El de la réplica');
         //Y, ahora sí, definimos el destinatario (dirección y, opcionalmente, nombre)
-        $mail->AddAddress('$_POST[email0]', '$_POST[nombre0]');
+        $mail->AddAddress($_POST['email0'], $_POST['nombre0']);
         //Definimos el tema del email
-        $mail->Subject = 'Esto es un correo de prueba';
+        $mail->Subject = 'Sistema Mantención';
         //Para enviar un correo formateado en HTML lo cargamos con la siguiente función. Si no, puedes meterle directamente una cadena de texto.
-        //$mail->MsgHTML(file_get_contents('correomaquetado.html'), dirname(ruta_al_archivo));
-        //Y por si nos bloquean el contenido HTML (algunos correos lo hacen por seguridad) una versión alternativa en texto plano (también será válida para lectores de pantalla)
-        $mail->Body = 'Hello, this is my message.';
-        $mail->AltBody = 'This is a plain-text message body';
-        //Enviamos el correo
+        $mail->MsgHTML("<html> 
+                            <head> 
+                                <title>Sistema Mantención</title> 
+                            </head> 
+                            <body> 
+                                <h1>Bienvenido ".$_POST['nombre0']."!</h1> 
+                                <p> 
+                                    <b>Para acceder al Sistema Mantención Landes S.A, pinche en el siguiente enlace:</b> 
+                                </p> 
+                                <p>
+                                    <a href='http://mantencion.landes.cl'>Sistema Mantención</a>
+                                </p>
+                                <p>
+                                    Contraseña temporal: ".$d."
+                                </p>
+                                <p>
+                                    Favor no responder este mensaje, Gracias!!.
+                                </p>
+                            </body> 
+                        </html>");
+        $mail->CharSet = 'UTF-8';
         if(!$mail->Send()) {
-        echo "Error: " . $mail->ErrorInfo;
+            $messages[] = "Usuario guardado satisfactoriamente.";
         } else {
-        echo "Enviado!";
+            echo $mail->ErrorInfo;
         }  
     }
 
