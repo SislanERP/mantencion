c0e35715abc41c39e7ed808c7106d4ad49fd857a
CASENJO1992 <casenjo@landes.cl>
2020-01-30 14:01:17 -0300

    se agrega favicon

--- head.php
+++ head.php
@@ -9,6 +9,7 @@
     <link rel="stylesheet" href="css/estilo.css"/>
     <link rel="stylesheet" href="css/ventas.css"/>
     <link href='css/fullcalendar.css' rel='stylesheet' />
+    <link rel="shortcut icon" href="img/favicon.png" />
     <!-- Latest compiled and minified CSS -->
     <link href="https://fonts.googleapis.com/css?family=Black+Ops+One|Carter+One&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-select/1.13.1/css/bootstrap-select.min.css">
