8394d604d0a2a9c8c0e2cd11615daee2ea697d86
CASENJO1992 <casenjo@landes.cl>
2020-02-03 15:00:04 -0300

    se prueba variable de ambiente

--- equipos.php
+++ equipos.php
@@ -36,6 +36,7 @@
     <?php include('nav.php');?>
 
     <div id="content">
+        <?php getenv("pass_correo");?>
         <div class="container-fluid">
             <div class="row justify-content-around movil">
                 <div class="datos">
