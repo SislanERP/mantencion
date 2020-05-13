ddbd56d6e6ef74189775d8552a7f5458170c9884
CASENJO1992 <casenjo@landes.cl>
2020-05-13 15:45:20 -0400

    se modifica ingreso de dias libres

--- php/acciones/add/add_libre.php
+++ php/acciones/add/add_libre.php
@@ -18,7 +18,6 @@
     }
 
     $consulta = "SELECT * FROM libres where fec_trabajada='".$_POST['fecha0']."' and id_usuario_registro=".$id_usuario;
-    echo $_POST['fecha0'].$id_usuario;
 	$resultado = mysqli_query( conectar(), $consulta );
 	if ($columna = mysqli_fetch_array( $resultado ))
 	{ 
