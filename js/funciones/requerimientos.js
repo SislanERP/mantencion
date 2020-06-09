df1b4a6fc078830add6cb6c452c9ffc8dcea24e0
casenjotorres <casenjo@landes.cl>
2020-06-09 14:27:01 -0400

    visualizacion de imagen en requerimiento

--- js/funciones/requerimientos.js
+++ js/funciones/requerimientos.js
@@ -49,8 +49,7 @@ function load(page){
         modal.find('select[id=prioridad1]').val(prioridad)
         modal.find('select[id=responsable1]').val(responsable)
         modal.find('.modal-body #desarrollo1').val(desarrollo)
-        document.getElementById("img-href").href = imagen;
-        document.getElementById("img-href").val = imagen;
+        modal.find('.modal-body #img-href').val(imagen)
 
         if(terminado == 1)
         {
