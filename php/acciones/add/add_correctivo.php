5d36aa5d06f8d5405b7dcf7477d281247e7aaa68
CASENJO1992 <casenjo@landes.cl>
2021-01-06 14:49:17 -0300

    correo correctivo test

--- php/acciones/add/add_correctivo.php
+++ php/acciones/add/add_correctivo.php
@@ -17,12 +17,87 @@
         $contador = 1;
     }
 
-    if(empty($_POST['ot_padre0'])){$ote="NULL";}else{$ote=$_POST['ot_padre0'];}
+    if(empty($_POST['ot_padre0'])){$ote="NULL"; $ot = "";}else{$ote=$_POST['ot_padre0']; $ot = $_POST['ot_padre0'];}
     
     $query="INSERT INTO correctivos (id_correctivo,ot_padre,fec_inicio,id_prioridad,id_tipo_mantenimiento,id_equipo,actividad,id_responsable,id_estado,fec_registro,id_usuario_registro) values($contador,$ote,'$_POST[fecha0]',$_POST[prioridad0],1,$_POST[equipo0],'$_POST[actividad0]',$_POST[responsable0],1,'$fecha',$id_usuario)";
     if (conectar()->query($query) === TRUE) 
     {
-        $messages[] = "Correctivo guardado satisfactoriamente.";
+        $consulta = "SELECT * FROM equipos where id_equipo=$_POST[equipo0] and log_pcc = 1";
+        $resultado = mysqli_query( conectar(), $consulta );
+        if ($columna = mysqli_fetch_array( $resultado ))
+        { 
+            $mail = new PHPMailer();
+            $mail->IsSMTP();
+            $mail->SMTPDebug  = 0;
+            $mail->Host       = 'smtp.gmail.com';
+            $mail->Port       = 465;
+            $mail->SMTPSecure = 'ssl';
+            $mail->SMTPAuth   = true;
+            $mail->Username   = "sismantencionlandes@gmail.com";
+            $mail->Password   = getenv("pass_correo");
+            $mail->SetFrom('sismantencionlandes@gmail.com', 'CORRECTIVO PROGRAMADO');
+
+            // $consulta = "CALL consulta_correctivo($contador)";
+            // $resultado = mysqli_query( conectar(), $consulta );
+            // while ($columna = mysqli_fetch_array( $resultado ))
+            // { 
+                //$mail->AddAddress($columna['email'], $columna['nombre']);
+            // }
+
+            $mail->AddAddress("casenjo@landes.cl", 'Cristian Asenjo');
+            $consulta = "CALL consulta_correctivo($contador)";
+            $resultado = mysqli_query( conectar(), $consulta );
+            if ($columna = mysqli_fetch_array( $resultado ))
+            { 
+                $mail->Subject = 'CORRECTIVO PROGRAMADO N° '.$contador;
+                $mail->MsgHTML("<html> 
+                                    <head> 
+                                        <title>Sistema Mantención</title> 
+                                    </head> 
+                                    <body> 
+                                        <h1>Se registro una mantención correctiva!</h1> 
+                                        <p> 
+                                            <h2>N°: ".$contador."</h2> 
+                                        </p> 
+                                        <p> 
+                                            <b>Fecha:</b></br>
+                                            <span>".$olumna['fecha']."</span>
+                                        </p> 
+                                        <p> 
+                                            <b>OT Padre:</b></br>
+                                            <span>".$columna['ot_padre']."</span>
+                                        </p> 
+                                        <p> 
+                                            <b>Prioridad:</b></br>
+                                            <span>".$columna['prioridad']."</span>
+                                        </p> 
+                                        <p> 
+                                            <b>Equipo:</b></br>
+                                            <span>".$columna['equipo']."</span>
+                                        </p> 
+                                        <p> 
+                                            <b>Responsable:</b></br>
+                                            <span>".$columna['responsable']."</span>
+                                        </p> 
+                                        <p> 
+                                            <b>Actividad:</b></br>
+                                            <span>".$columna['actividad']."</span>
+                                        </p> 
+                                        <p>
+                                            Favor no responder este mensaje, Gracias!!.
+                                        </p>
+                                    </body> 
+                                </html>");
+                $mail->CharSet = 'UTF-8';
+                if(!$mail->Send()) {
+                    $messages[] = "Correctivo guardado satisfactoriamente.";
+                } else {
+                    echo $mail->ErrorInfo;
+                }  
+            }
+
+            
+        }
     }
 
     else
