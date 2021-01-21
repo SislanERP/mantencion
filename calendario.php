b1f2ffcbb672ef30cc511c5d9dd3dda0d2251391
CASENJO1992 <casenjo@landes.cl>
2021-01-21 10:13:35 -0300

    modificiacion fecha reporte de equipos

--- calendario.php
+++ calendario.php
@@ -134,7 +134,7 @@
 			<div class="modal-content">
 			<form class="form-horizontal" id="CrearPreventivo">
             <div class="modal-header">
-                    <h5 class="modal-title" id="exampleModalLabel">Crear Preventiva </h5>
+                    <h5 class="modal-title" id="exampleModalLabel">Crear Preventiva</h5>
                     <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                         <span aria-hidden="true">&times;</span>
                     </button>
