e7e1d3c71643cbc3aedf449cbf3637bd3dfdfd8c
CASENJO1992 <casenjo@landes.cl>
2021-02-10 10:21:00 -0300

    correccion plc

--- ajax/consulta_flujometro.php
+++ ajax/consulta_flujometro.php
@@ -6,14 +6,14 @@
 	$resultado = mysqli_query( conectar(), $consulta );
 	while ($columna = mysqli_fetch_array( $resultado ))
 	{
-        $consulta = "call consulta_plc($columna[id_registro])";
-        $resultado = mysqli_query( conectar(), $consulta );
-        while ($columna = mysqli_fetch_array( $resultado ))
+        $consulta1 = "call consulta_plc($columna[id_registro])";
+        $resultado1 = mysqli_query( conectar(), $consulta1 );
+        if ($columna1 = mysqli_fetch_array( $resultado1 ))
         {
             echo "
                 <div class='col-lg-4 col-sm-12 mb-4'>
-                    <h2 class='plc_titulo'>".$columna['nombre']."</h2>
-                    <h1 class='plc_valor'>".$columna['valor']."</h1>
+                    <h2 class='plc_titulo'>".$columna1['nombre']."</h2>
+                    <h1 class='plc_valor'>".$columna1['valor']."</h1>
                 </div>
             ";
         }
