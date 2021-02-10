601a3a6a30a6486bd8ffd0e16297bc3f77ae117c
CASENJO1992 <casenjo@landes.cl>
2021-02-10 10:14:10 -0300

    correccion plc

--- ajax/consulta_flujometro.php
+++ ajax/consulta_flujometro.php
@@ -2,16 +2,23 @@
     session_start();
     require_once("../php/conexion.php");
 
-    $consulta = "call consulta_plc()";
+    $consulta = "SELECT * FROM memorias WHERE id_plc = 1";
 	$resultado = mysqli_query( conectar(), $consulta );
 	while ($columna = mysqli_fetch_array( $resultado ))
 	{
-        echo "
-            <div class='col-lg-4 col-sm-12 mb-4'>
-                <h2 class='plc_titulo'>".$columna['nombre']."</h2>
-                <h1 class='plc_valor'>".$columna['valor']."</h1>
-            </div>
-        ";
+        $consulta = "call consulta_plc($columna[id_registro])";
+        $resultado = mysqli_query( conectar(), $consulta );
+        while ($columna = mysqli_fetch_array( $resultado ))
+        {
+            echo "
+                <div class='col-lg-4 col-sm-12 mb-4'>
+                    <h2 class='plc_titulo'>".$columna['nombre']."</h2>
+                    <h1 class='plc_valor'>".$columna['valor']."</h1>
+                </div>
+            ";
+        }
     }
+
+    
 
 ?>
