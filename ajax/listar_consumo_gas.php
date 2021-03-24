d2765499c754d3f9af7dbf66950ebbaef2d2079b
CASENJO1992 <casenjo@landes.cl>
2021-03-24 08:37:55 -0300

    se agrega reporte de caldera

---
+++ ajax/listar_consumo_gas.php
@@ -0,0 +1,107 @@
+<?php
+	session_start();
+	require_once("../php/conexion.php");
+
+	$id_usuario = $_SESSION['id_user'];
+		
+    $desde = mysqli_real_escape_string(conectar(),(strip_tags($_REQUEST['desde'], ENT_QUOTES)));
+    $hasta = mysqli_real_escape_string(conectar(),(strip_tags($_REQUEST['hasta'], ENT_QUOTES)));
+
+    $_SESSION['totalizador_gas_desde'] = $desde;
+    $_SESSION['totalizador_gas_hasta'] = $hasta;
+
+    $tables="   caldera a inner join 
+                detalle_caldera b on a.id_control = b.id_control ";
+
+    $campos="   a.fecha as fecha,
+                a.hora_encendido as inicio,
+                a.hora_apagado as detencion,
+                (select x1.entrada from detalle_caldera x1 where x1.id_control = a.id_control and x1.id_tipo = 2 order by x1.id_registro asc limit 1) as entrada,
+                (select x2.salida from detalle_caldera x2 where x2.id_control = a.id_control and x2.id_tipo = 2  order by x2.id_registro desc limit 1) as salida";
+	$sWhere="   b.id_tipo = 2 and date(a.fecha) between '".$desde."' and '".$hasta."'";
+	$sWhere.="  group by a.fecha";
+
+    include 'pagination.php'; 
+
+    $page = getPaginationPos();
+	$per_page = 10;
+    $adjacents  = 10;
+    $offset = ($page - 1) * $per_page;
+    $count_query   = mysqli_query(conectar(),"SELECT count(*) AS numrows FROM $tables where $sWhere");
+    if ($row= mysqli_fetch_array($count_query)){$numrows = $row['numrows'];}
+    $total_pages = ceil($numrows/$per_page);
+	$reload = 'consulta_caldera.php';
+
+    $query = mysqli_query(conectar(),"SELECT $campos from $tables where $sWhere Limit $offset,$per_page");
+
+    if ($numrows>0){
+        $consulta = "SELECT  min(a.valor) as minimo , max(a.valor) as maximo FROM f_total_riles a WHERE date(a.fecha) between '$desde' and '$hasta'";
+        $resultado = mysqli_query(conectar(), $consulta );
+        if($row = mysqli_fetch_array($resultado)){
+            $total = $row['maximo'] - $row['minimo'];
+        }
+?>
+        <div class="d-flex justify-content-between">
+            <h1>Totalizador: <?=$total?> m³</h1>
+            <a href="diseños/consultas/reporte_totalizador.php" class="btn btn-primary agregar e6">
+                <img src="img/iconos/pdf.svg" alt="" style="width:34px; margin-right: 14px;"> Exportar
+            </a>
+        </div>
+	    <table class="table">
+			  <thead class="thead-light">
+				<tr>
+				  <th>Fecha</th>
+				  <th>Hora encendido</th>
+                  <th>Hora apagado</th>
+                  <th>Entrada m³</th>
+                  <th>Salida m³</th>
+                  <th>Total día m³</th>
+				</tr>
+			</thead>
+			<tbody id="myTable">
+			<?php
+			while($row = mysqli_fetch_array($query)){
+				?>
+				<tr>
+					<td><?=$row['fecha'];?></td>
+					<td><?=$row['inicio']?></td>
+                    <td><?=$row['detencion']?></td>
+                    <td><?=$row['entrada']?> m³</td>
+                    <td><?=$row['salida']?> m³</td>
+                    <td><?=$row['salida'] - $row['entrada']?> m³</td>
+				</tr>
+				<?php
+			}
+			?>
+			</tbody>
+		</table>
+		<div class="table-pagination pull-right">
+			<?php echo paginate($reload, $page, $total_pages, $adjacents);?>
+		</div>
+		
+			<?php
+			
+		} else {
+			?>
+			<div class="alert alert-warning alert-dismissable mt-3 w-100">
+              <button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
+              <h4>Aviso!!!</h4> No hay datos para mostrar
+            </div>
+			<?php
+		}
+
+        function getPaginationPos(){
+            if (isset($_REQUEST['page']) && !empty($_REQUEST['page']))
+            {
+                setcookie('page_caldera_gas',$_REQUEST['page'],time() + 86400);
+                return $_REQUEST['page'];
+            } 
+            else 
+            {
+                return ($_COOKIE['page_caldera_gas']!='' ? $_COOKIE['page_caldera_gas'] : 1);
+            }
+            
+        }
+?>
+
+		  
---
+++ consulta_caldera.php
@@ -0,0 +1,101 @@
+<?php 
+    include('php/funciones.php');
+?>
+
+<?php
+    $inactivo = 1800;
+ 
+    if(isset($_SESSION['id_user']) ) {
+        $vida_session = time() - $_SESSION['tiempo'];
+        if($vida_session > $inactivo)
+        {
+            session_destroy();
+            echo "<script>location.href='index.php';</script>";
+            die();
+        }
+        else
+        {
+            $_SESSION['tiempo'] = time();
+        }
+    }
+    else
+    {
+        echo "<script>location.href='index.php';</script>";
+        die();
+    }
+?>
+
+<!DOCTYPE html>
+<html lang="es">
+    <?php include('head.php');?>
+<body>
+    <?php include('nav.php');?>
+
+    <div id="content">
+      <div class="content-fluid p-5 shadow mb-5 bg-white e7" style="background:#fff;border-radius:15px;">
+        <div class="d-flex justify-content-between e3">
+          <h3>Consulta Caldera</h3>
+        </div>
+        <div class="row">
+            <div class="col-lg-3">
+                <div class="form-group mb-0">
+                    <label class="col-form-label">Desde:</label>
+                    <input type="date" class="form-control" id="desde" name="desde" value="<?=date("Y-m-d")?>">
+                </div>
+            </div>
+            <div class="col-lg-3">
+                <div class="form-group mb-0">
+                    <label class="col-form-label">Hasta:</label>
+                    <input type="date" class="form-control" id="hasta" name="hasta" value="<?=date("Y-m-d")?>">
+                </div>
+            </div>
+            <div class="col-lg-2 d-flex align-items-end">
+                <a href="#" id="Buscar" class="btn btn-primary agregar e6">
+                    <img src="img/iconos/buscar.svg" alt="" style="width:34px; margin-right: 14px;"> Buscar
+                </a>
+            </div>
+        </div>
+        <nav class="mt-4">
+            <div class="nav nav-tabs" id="nav-tab" role="tablist">
+                <button class="nav-link active" id="nav-riles-tab" data-bs-toggle="tab" data-bs-target="#nav-riles" type="button" role="tab" aria-controls="nav-home" aria-selected="true">Consumo Gas</button>
+            </div>
+        </nav>
+        <div class="tab-content" id="nav-tabContent">
+            <div class="tab-pane fade show active" id="nav-riles" role="tabpanel" aria-labelledby="nav-riles-tab">
+                <div class="riles mt-3 d-flex flex-column">
+                
+                </div> 
+            </div>
+        </div>
+         
+    </div>
+  </div>
+              
+  <?php include('footer.php');?>
+  <script>
+		$(document).ready(function(){
+            load(1);
+		});
+
+        function load(page){
+            var desde = $("#desde").val();
+            var hasta = $("#hasta").val();
+            var parametros = {"desde": desde,"hasta": hasta, "page":page};
+            $.ajax({
+                url:'ajax/listar_totalizador.php',
+                data: parametros,
+                beforeSend: function(objeto){
+                    $("#loader").html("<img src='img/loader.gif'>");
+                },
+                success:function(data){
+                    $(".riles").html(data).fadeIn('slow');
+                }
+            })
+        }
+
+        $( "#Buscar" ).click(function() {
+            load(1);
+        });
+    </script>
+</body>
+</html>
