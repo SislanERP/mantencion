fb1def0bc6222aa29240c727f60c020c83dd38ef
CASENJO1992 <casenjo@landes.cl>
2021-05-18 11:21:28 -0400

    se agrega nuevas frecuencia al calendario

--- php/acciones/add/add_evento.php
+++ php/acciones/add/add_evento.php
@@ -8,43 +8,43 @@ $fecha_modificada = date("Y-m-d");
 $fec_inicio = $_POST['start'];
 $mes_actual = date("m",strtotime($_POST['start']));
 
-while($fecha_modificada < $año_siguiente)
+if($_POST['frecuencia'] == 1)
+{ 
+    $frecuencia = "days";
+}
+
+if($_POST['frecuencia'] == 2)
+{ 
+    $frecuencia = "week";
+}
+
+if($_POST['frecuencia'] == 3)
+{ 
+    $frecuencia = "month";
+}
+
+if($_POST['frecuencia'] == 4)
+{ 
+    $frecuencia = "year";
+}
+
+if($_POST['frecuencia'] == 5)
+{ 
+    $frecuencia = "month";
+}
+
+if($_POST['frecuencia'] == 6)
+{ 
+    $frecuencia = "month";
+}
+
+if($_POST['frecuencia'] == 7)
+{ 
+    $frecuencia = "month";
+}
+
+while($_POST['start'] < $año_siguiente)
 {
-    if($_POST['frecuencia'] == 1)
-    { 
-        $frecuencia = "days";
-    }
-
-    if($_POST['frecuencia'] == 2)
-    { 
-        $frecuencia = "week";
-    }
-
-    if($_POST['frecuencia'] == 3)
-    { 
-        $frecuencia = "month";
-    }
-
-    if($_POST['frecuencia'] == 4)
-    { 
-        $frecuencia = "year";
-    }
-
-    if($_POST['frecuencia'] == 5)
-    { 
-        $frecuencia = "month";
-    }
-
-    if($_POST['frecuencia'] == 6)
-    { 
-        $frecuencia = "month";
-    }
-
-    if($_POST['frecuencia'] == 7)
-    { 
-        $frecuencia = "month";
-    }
-
     $consulta = "SELECT max(id) as correlativo FROM events";
     $resultado = mysqli_query( conectar(), $consulta );
     if ($columna = mysqli_fetch_array( $resultado ))
@@ -56,82 +56,64 @@ while($fecha_modificada < $año_siguiente)
         $contador = 1;
     }
 
-    $consulta = "SELECT * FROM events WHERE title = $_POST[title] and YEAR(start) = YEAR(now())";
-    $resultado = mysqli_query( conectar(), $consulta );
-    if ($columna = mysqli_fetch_array( $resultado ))
-    { 
-        $query="INSERT INTO events (id,title,start,end,color) values($contador,'$_POST[title]','$fec_inicio','$fec_inicio','#FF0000')";
-        if (conectar()->query($query) === TRUE) 
-        {
-            $messages[] = "Preventivo guardado satisfactoriamente.";
-        }
-
-        else
-        {
-            $errors []= "Lo siento algo ha salido mal intenta nuevamente.".mysqli_error(conectar());
-        }
+    $query="INSERT INTO events (id,title,start,end,color) values($contador,'$_POST[title]','$fecha_modificada','$fecha_modificada','#FF0000')";
+    if (conectar()->query($query) === TRUE) 
+    {
+        $messages[] = "Preventivo guardado satisfactoriamente.";
     }
+
     else
     {
-        $query="INSERT INTO events (id,title,start,end,color) values($contador,'$_POST[title]','$_POST[start]','$_POST[start]','#FF0000')";
-        if (conectar()->query($query) === TRUE) 
-        {
-            $messages[] = "Preventivo guardado satisfactoriamente.";
-        }
-
-        else
-        {
-            $errors []= "Lo siento algo ha salido mal intenta nuevamente.".mysqli_error(conectar());
-        }
+        $errors []= "Lo siento algo ha salido mal intenta nuevamente.".mysqli_error(conectar());
     }
-
+    
     if($_POST['frecuencia'] == 5)
     { 
+        $mes_actual = $mes_actual + 2;
         $fecha_modificada = strtotime ('+'.$mes_actual.''.$frecuencia, strtotime($_POST['start']));
         $fec_inicio =  date("Y-m-d",$fecha_modificada);
-        $mes_actual = $mes_actual + 2;
     }
 
     if($_POST['frecuencia'] == 6)
     { 
+        $mes_actual = $mes_actual + 3;
         $fecha_modificada = strtotime ('+'.$mes_actual.''.$frecuencia, strtotime($_POST['start']));
         $fec_inicio =  date("Y-m-d",$fecha_modificada);
-        $mes_actual = $mes_actual + 3;
     }
 
     if($_POST['frecuencia'] == 7)
     { 
+        $mes_actual = $mes_actual + 6;
         $fecha_modificada = strtotime ('+'.$mes_actual.''.$frecuencia, strtotime($_POST['start']));
         $fec_inicio =  date("Y-m-d",$fecha_modificada);
-        $mes_actual = $mes_actual + 6;
     }
 
     if($_POST['frecuencia'] == 4)
     { 
+        $mes_actual = $mes_actual + 1;
         $fecha_modificada = strtotime ('+'.$mes_actual.''.$frecuencia, strtotime($_POST['start']));
         $fec_inicio =  date("Y-m-d",$fecha_modificada);
-        $mes_actual = $mes_actual + 1;
     }
 
     if($_POST['frecuencia'] == 3)
     { 
+        $mes_actual = $mes_actual + 1;
         $fecha_modificada = strtotime ('+'.$mes_actual.''.$frecuencia, strtotime($_POST['start']));
         $fec_inicio =  date("Y-m-d",$fecha_modificada);
-        $mes_actual = $mes_actual + 1;
     }
 
     if($_POST['frecuencia'] == 2)
     { 
+        $mes_actual = $mes_actual + 1;
         $fecha_modificada = strtotime ('+'.$mes_actual.''.$frecuencia, strtotime($_POST['start']));
         $fec_inicio =  date("Y-m-d",$fecha_modificada);
-        $mes_actual = $mes_actual + 1;
     }
 
     if($_POST['frecuencia'] == 1)
     { 
+        $mes_actual = $mes_actual + 1;
         $fecha_modificada = strtotime ('+'.$mes_actual.''.$frecuencia, strtotime($_POST['start']));
         $fec_inicio =  date("Y-m-d",$fecha_modificada);
-        $mes_actual = $mes_actual + 1;
     }
 }
 
